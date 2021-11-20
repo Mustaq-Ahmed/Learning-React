b39eb687797621fb75c3483f5776f0d8fbd97c26
Mustaq <mustaq.ahmed.codes@gmail.com>
2021-11-21 02:08:42 +0530

    Providing Alert For Actions

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 
 function App() {
-  function toggleMode(){
-    if(mode === "light"){
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  function showAlert(type, message) {
+    setAlert({
+      type: type,
+      msg: message
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  function toggleMode() {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#0d0d33"
+      showAlert("success", "Dark mode has been enabled")
     }
-    else{
+    else {
       setMode("light")
       document.body.style.backgroundColor = "white"
+      showAlert("success", "Light mode has been enabled")
     }
   }
-  const [mode, setMode] = useState("light");
+
+
   return (
     <>
-      <Navbar title="TextUtils" about="About Us" mode={mode} toggleMode={toggleMode}/>
+      <Navbar title="TextUtils" about="About Us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading = "Enter Text to Analyze" mode={mode}/>
+        <TextForm heading="Enter Text to Analyze" mode={mode} showAlert={showAlert} />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,31 +1,50 @@
 import React, {useState} from 'react'
 
+
 export default function TextForm(props) {
     function handleOnChange(event){
-        console.log("On Change");
+        // console.log("On Change");
         setText(event.target.value)  // setting text
     }
     
     function handleOnUpClick(){
         // console.log("UpperCase Button was Clicked " + text);
         setText(text.toUpperCase())  //using text from handleOnChange function and converting to uppercase.Note text iscoming from above function
+        props.showAlert("success", "Text is Converted to UpperCase")
     }
     function handleOnLowClick(){
         // console.log("LowerCase Button was Clicked " + text);
         setText(text.toLowerCase())
+        props.showAlert("success", "Text is Converted to LowerCase")
+
     }
     function handleOnClearClick(){
         setText("")
+        props.showAlert("success", "Text Cleared")
     }
     function handleCopy(){
         let content = document.getElementById("mybox");
         content.select();
         navigator.clipboard.writeText(content.value);
+        props.showAlert("success", "Copied to Clipboard")
+
     }
     function handleExtraSpaces(){
         let newText = text.split(/[" "]+/); // Split Text Sentences by one/more Spaces. => returns ARRAY
         setText(newText.join(" "));  // join array items by spaces and returns a string.
+        props.showAlert("success", "Extra Spaces are Removed")
     }
+    function handleCapitalize(){
+        let arr = [];
+        let words = text.split(" ");
+        for(let word of words){
+            let lower = word.toLowerCase();
+            arr.push(lower.charAt(0).toUpperCase() + lower.slice(1));
+        }
+        setText(arr.join(" "))
+        props.showAlert("success", "Words Capitalize")
+    }
+        
     const [text, setText] = useState("")  // Default value is blank text (no text)
 
     return (
@@ -40,6 +59,7 @@ export default function TextForm(props) {
             <button type="button" className="btn btn-primary mx-2" onClick={handleOnClearClick} >Clear Text</button>
             <button type="button" className="btn btn-primary mx-2" onClick={handleCopy} >Copy Text</button>
             <button type="button" className="btn btn-primary mx-2" onClick={handleExtraSpaces} >Remove ExtraSpaces</button>
+            <button type="button" className="btn btn-primary mx-2" onClick={handleCapitalize} >Capitalize</button>
         </div>
         <div className="container my-3" style={{color: props.mode === "light"?"black":"white"}}>
             <h2> Text Summary</h2>
